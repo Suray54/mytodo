ba8cb912c761ac0c137c6e109f3a044db13939f4
Suranjan Rai <Suranjanthulungrai@gmail.com>
2020-10-19 12:40:34 +0545

    Updates

---
+++ precache-manifest.24183f739e9891c4926fe10d91e2c7a7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a47a954d874ad4971ce74cda371be0c9",
+    "url": "/mytodo/index.html"
+  },
+  {
+    "revision": "ab81336ed4971001abd5",
+    "url": "/mytodo/static/css/main.ab0d45ec.chunk.css"
+  },
+  {
+    "revision": "db2accd6b5561c01e5e9",
+    "url": "/mytodo/static/js/2.af8298d4.chunk.js"
+  },
+  {
+    "revision": "2895da7ef1072e39c0ea4881225c718c",
+    "url": "/mytodo/static/js/2.af8298d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab81336ed4971001abd5",
+    "url": "/mytodo/static/js/main.e7c98e39.chunk.js"
+  },
+  {
+    "revision": "98cf56292a6443c94f00",
+    "url": "/mytodo/static/js/runtime-main.f1022883.js"
+  },
+  {
+    "revision": "41c090e1dc4d4efd8415943ca03807e6",
+    "url": "/mytodo/static/media/logowhite.41c090e1.png"
+  }
+]);
